86d2b1f6dee243f792e3fbd55fb674fa0181f736
Arul.Nambi <arul.nambi@amdocs.com>
2018-11-06 14:37:32 -0500

    Update event.client.version
    Issue-ID: AAI-1877
    Change-Id: Ia02b0af389af94a95619be559c90cdf4ca0aca9e
    Signed-off-by: Arul.Nambi <arul.nambi@amdocs.com>

--- src/main/java/org/onap/aai/util/Version.java
+++ src/main/java/org/onap/aai/util/Version.java
@@ -21,5 +21,5 @@
 package org.onap.aai.util;
 
 public enum Version {
-  v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12, v13, v14, v15, v16, v17, v18, v19, v20;
+  v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12, v13, v14, v15, v16, v17, v18, v19, v20;
 }
